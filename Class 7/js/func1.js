85aa84c2875e1530e70da8dd02f0f1dc43c3311a
Nicolas <na796840@gmail.com>
2023-03-02 20:11:39 -0500

    class

---
+++ Class 7/js/func1.js
@@ -0,0 +1,19 @@
+let x = $(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos() {
+  let x = $("#boton1");
+  x.click(ocultarRecuadro);
+  x = $("#boton2");
+  x.click(mostrarRecuadro);
+}
+
+function ocultarRecuadro() {
+  let x = $("#descripcion");
+  x.hide("slow");
+}
+
+function mostrarRecuadro() {
+  let x = $("#descripcion");
+  x.show("fast");
+}
