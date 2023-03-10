308f6dcc39f4a9ffd3f357ac9b6d48f8e1b5e1e6
Nicolas <na796840@gmail.com>
2023-03-09 22:10:30 -0500

    class

--- Segundo Examen/js/pregunta9.js
+++ Segundo Examen/js/pregunta9.js
@@ -3,7 +3,7 @@ var filas = document.querySelectorAll('#tabla1 tr');
 // Agregar evento a cada fila de la primer tabla
 filas.forEach(function(fila) {
     fila.addEventListener('click', function() {
-        // Cambiar clase de la fila seleccionada
+        
         fila.classList.toggle('seleccionado');
     });
 });
